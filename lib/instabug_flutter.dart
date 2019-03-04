5f485150f590ca13c74009e6818ca61286235b93
Aly ezz <alyezz94@gmail.com>
2019-03-04 10:37:28 +0200

    ✨ [Flutter] adding identify user with email Api

--- lib/instabug_flutter.dart
+++ lib/instabug_flutter.dart
@@ -31,6 +31,7 @@ class InstabugFlutter {
     Map params = {'token': token, 'invocationEvents': invocationEventsStrings};
     await _channel.invokeMethod('startWithToken:invocationEvents:', params);
   }
+
    /*
    * Shows the welcome message in a specific mode.
    * @param welcomeMessageMode An enum to set the welcome message mode to
@@ -40,6 +41,18 @@ class InstabugFlutter {
   static void showWelcomeMessageWithMode(WelcomeMessageMode welcomeMessageMode) async {
     Map params = {'welcomeMessageMode': welcomeMessageMode.toString() };
     await _channel.invokeMethod('showWelcomeMessageWithMode:', params);
+  }
+  /*
+   * Sets the default value of the user's email and hides the email field from the reporting UI
+   * and set the user's name to be included with all reports.
+   * It also reset the chats on device to that email and removes user attributes,
+   * user data and completed surveys.
+   * @param {string} email Email address to be set as the user's email.
+   * @param {string} name Name of the user to be set.
+   */
+  static void identifyUserWithEmail(String email, String name) async {
+    Map params = {'email': email, 'name': name };
+    await _channel.invokeMethod('identifyUserWithEmail:name:', params);
   }
   
 }
