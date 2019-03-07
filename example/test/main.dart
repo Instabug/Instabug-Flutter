d120f58fa0f07d5f18350780c49eb567300bb795
Aly ezz <alyezz94@gmail.com>
2019-03-07 17:41:04 +0200

    ✨ Adds test for showWelcomeMessageWithMode:

---
+++ example/test/main.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:instabug_flutter/instabug_flutter.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  final List<MethodCall> log = <MethodCall>[];
+  final appToken = '068ba9a8c3615035e163dc5f829c73be';
+  final invocationEvents = [InvocationEvent.floatingButton];
+  final email = "s@nta.com";
+  final name = "santa";
+
+  setUpAll(() async {
+    MethodChannel('instabug_flutter')
+        .setMockMethodCallHandler((MethodCall methodCall) async {
+      log.add(methodCall);
+      switch (methodCall.method) {
+        case 'showWelcomeMessageWithMode:':
+          return null;
+        default:
+          return null;
+      }
+    });
+  });
+
+  tearDown(() async {
+      log.clear();
+    });
+
+  test('showWelcomeMessageWithMode: Test', () async {
+    InstabugFlutter.showWelcomeMessageWithMode(WelcomeMessageMode.beta);
+    expect(log, <Matcher>[
+      isMethodCall('showWelcomeMessageWithMode:',
+        arguments: <String, dynamic>{
+          'welcomeMessageMode': WelcomeMessageMode.beta.toString()
+        },
+      )
+    ]);
+  });
+}
