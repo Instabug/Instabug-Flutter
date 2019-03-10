02ee3fbbe422e816f2f8e151b98c9437d7b940d1
Aly ezz <alyezz94@gmail.com>
2019-03-10 13:16:43 +0200

    ✨[Example app] calling logDebug from flutter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,6 +30,7 @@ class _MyAppState extends State<MyApp> {
       }
       InstabugFlutter.showWelcomeMessageWithMode(WelcomeMessageMode.beta);
       InstabugFlutter.identifyUserWithEmail("aezz@instabug.com", "Aly Ezz");
+      InstabugFlutter.logDebug("Test Debug Message from Flutter");
       InstabugFlutter.logOut();
       InstabugFlutter.setLocale(Locale.German);
 
