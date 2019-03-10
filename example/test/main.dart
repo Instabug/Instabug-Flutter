40be251c48f50139de4a133a2aabe1c00588eaa4
Aly Ezz <alyezz94@gmail.com>
2019-03-10 12:24:13 +0200

    ✨ Adds Test for startWithToken:invocationEvents: (#13)

---
+++ example/test/main.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:instabug_flutter/instabug_flutter.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  final List<MethodCall> log = <MethodCall>[];
+  final appToken = '068ba9a8c3615035e163dc5f829c73be';
+  final invocationEvents = [InvocationEvent.floatingButton];
+  final email = "s@nta.com";
+  final name = "santa";
+
+  setUpAll(() async {
+    MethodChannel('instabug_flutter')
+        .setMockMethodCallHandler((MethodCall methodCall) async {
+      log.add(methodCall);
+      switch (methodCall.method) {
+        case 'startWithToken:invocationEvents:':
+          return null;
+        default:
+          return null;
+      }
+    });
+  });
+
+  tearDown(() async {
+      log.clear();
+    });
+
+  test('startWithToken:invocationEvents: Test', () async {
+    InstabugFlutter.start(appToken, invocationEvents);
+    expect(log, <Matcher>[
+      isMethodCall('startWithToken:invocationEvents:',
+        arguments: <String, dynamic>{
+          'token': appToken,
+          'invocationEvents': [InvocationEvent.floatingButton.toString()]
+        },
+      )
+    ]);
+  });
+}
