46df6d3c337687794158f34d8c5d302f36cd176d
Aly ezz <alyezz94@gmail.com>
2019-03-11 13:33:27 +0200

    ✨[Tests] Adding clearLogs Testing

--- example/test/main.dart
+++ example/test/main.dart
@@ -33,6 +33,8 @@ void main() {
         case 'logDebug:':
           return null; 
         case 'logInfo:':
+          return null; 
+        case 'clearAllLogs:':
           return null;    
         default:
           return null;
@@ -141,6 +143,16 @@ test('startWithToken:invocationEvents: Test', () async {
       isMethodCall('logInfo:',
         arguments: <String, dynamic>{
           'message': message
+        },
+      )
+    ]);
+  });
+
+  test('clearAllLogs: Test', () async {
+    InstabugFlutter.clearAllLogs();
+    expect(log, <Matcher>[
+      isMethodCall('clearAllLogs',
+        arguments: <String, dynamic>{
         },
       )
     ]);
