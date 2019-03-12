9e1dd05056ca71e5534a377998a3428e37d41703
Salma Ali <bluet.salma@gmail.com>
2019-03-12 14:16:35 +0200

    [MOB 2873] Flutter API: resetTags (#27)
    * ✨ Add android map setColorTheme
    
    * ✨ Add ios map for setColorTheme
    
    * ✨ Add dart setColorTheme API
    
    * ✨ Add setColorTheme to example app
    
    * ✨ add setColorTheme test case
    
    * ✨ add android map for appendTags
    
    * ✨ add ios map for appendTags
    
    * ✨ add dart API appendTags
    
    * ✨ add appendTags to the example app
    
    * 🔥 revert change to app token in example app
    
    * ✨ add test for API appendTags
    
    * 📝 Change the documentation method of the Dart APIs to conform to Dart's recommended library documentation
    
    * ✨ Add android map for resetTags
    
    * 🐛 Fix crash on calling an android mapped API with no arguments
    
    * ✨ Add ios map for resetTags
    
    * ✨ Add dart API for resetTags
    
    * ✨ Add button to invoke resetTags in the example app
    
    * ✨ Add resetTags test
    
    * 📚Add implemented APIs to the docs
    
    * 🐛 fix to type

--- android/src/main/java/com/instabug/instabugflutter/InstabugFlutterPlugin.java
+++ android/src/main/java/com/instabug/instabugflutter/InstabugFlutterPlugin.java
@@ -67,7 +67,10 @@ public class InstabugFlutterPlugin implements MethodCallHandler {
       if (callMethod.equals(method.getName())) {
         isImplemented = true;
         ArrayList<Object> tempParamValues = new ArrayList<>();
-        HashMap map = (HashMap<String, String>)call.arguments;
+        HashMap<String, String> map = new HashMap<>();
+        if (call.arguments != null) {
+          map = (HashMap<String, String>) call.arguments;
+        }
         Iterator iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
           Map.Entry pair = (Map.Entry)iterator.next();
@@ -194,6 +197,13 @@ public class InstabugFlutterPlugin implements MethodCallHandler {
      */
    public void appendTags(ArrayList<String> tags) {
        Instabug.addTags(tags.toArray(new String[0]));
+   }
+
+  /**
+   * Manually removes all tags of reported feedback, bug or crash.
+   */
+  public void resetTags() {
+     Instabug.resetTags();
    }
 
   public Map<String, Object> getConstants() {
--- ios/Classes/InstabugFlutterPlugin.m
+++ ios/Classes/InstabugFlutterPlugin.m
@@ -150,6 +150,13 @@
     [Instabug appendTags:tags];
 }
 
+/**
+ * Manually removes all tags of reported feedback, bug or crash.
+ */
++ (void)resetTags {
+    [Instabug resetTags];
+}
+
 + (NSDictionary *)constants {
   return @{
       @"InvocationEvent.shake": @(IBGInvocationEventShake),
