45e4e61b4d33ac4f9f58a8d4f9040fa3e35a55a6
Aly ezz <alyezz94@gmail.com>
2019-03-20 10:23:09 +0200

    📝 Trying new config

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
-pwd
 VERSION=v$(egrep -o "version: ([0-9]-*.*)+[0-9]" pubspec.yaml | cut -d ":" -f 2)
-echo ${VERSION}
-# if [ ! "${VERSION}" ] || [ -z "${VERSION}" ];then
-#     echo "Instabug: err: Version Number not found."
-#     exit 1
-# else 
-#     pub global activate grinder
-#     flutter packages get
-#     grind auto-publish
-#     OWNER="Instabug"
-#     REPOSITORY="Instabug-Flutter"
-#     ACCESS_TOKEN=${RELEASE_GITHUB_TOKEN}
-#     VERSION=$(echo ${VERSION} | sed 's/ //1')
-#     echo ${VERSION}
-#     curl --data '{"tag_name": "'$VERSION'",
-#                 "target_commitish": "master",
-#                 "name": "'$VERSION'",
-#                 "body": "Release of version '$VERSION'",
-#                 "draft": false,
-#                 "prerelease": false}'  https://api.github.com/repos/$OWNER/$REPOSITORY/releases?access_token=$ACCESS_TOKEN
-#     echo "https://api.github.com/repos/$OWNER/$REPOSITORY/releases?access_token=$ACCESS_TOKEN"
-# fi
+if [ ! "${VERSION}" ] || [ -z "${VERSION}" ];then
+    echo "Instabug: err: Version Number not found."
+    exit 1
+else 
+    pub global activate grinder
+    flutter packages get
+    grind auto-publish
+    OWNER="Instabug"
+    REPOSITORY="Instabug-Flutter"
+    ACCESS_TOKEN=${RELEASE_GITHUB_TOKEN}
+    VERSION=$(echo ${VERSION} | sed 's/ //1')
+    echo ${VERSION}
+    curl --data '{"tag_name": "'$VERSION'",
+                "target_commitish": "master",
+                "name": "'$VERSION'",
+                "body": "Release of version '$VERSION'",
+                "draft": false,
+                "prerelease": false}'  https://api.github.com/repos/$OWNER/$REPOSITORY/releases?access_token=$ACCESS_TOKEN
+    echo "https://api.github.com/repos/$OWNER/$REPOSITORY/releases?access_token=$ACCESS_TOKEN"
+fi
