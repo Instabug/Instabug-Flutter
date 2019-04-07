e96216703e2203d74c0b57ae1010db71d6537be2
Aly ezz <alyezz94@gmail.com>
2019-04-08 01:59:21 +0200

    ✨ Flutter API Mappings

--- lib/BugReporting.dart
+++ lib/BugReporting.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:instabug_flutter/Instabug.dart';
 
@@ -10,12 +11,31 @@ enum InvocationOption {
 }
 
 class BugReporting {
+
+  static Function onInvokeCallback;
+  static Function onDismissCallback;
   static const MethodChannel _channel = MethodChannel('instabug_flutter');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static Future<dynamic> _handleMethod(MethodCall call) async {
+  switch(call.method) {
+    case 'onInvokeCallback':
+      onInvokeCallback();
+      return ;
+    case 'onDismissCallback':
+      try {
+        onDismissCallback(call.arguments);
+      }
+      catch(exception) {
+        onDismissCallback();
+      }
+      return ;
+  }
+}
   /// invoke sdk manually with desire invocation mode
   /// [invocationMode] the invocation mode
   /// [invocationOptions] the array of invocation options
@@ -29,4 +49,31 @@ class BugReporting {
     final List<dynamic> params = <dynamic>[invocationMode.toString(), invocationOptionsStrings];
     await _channel.invokeMethod<Object>('invokeWithMode:options:',params);
   }
+
+  ///Enables and disables manual invocation and prompt options for bug and feedback.
+  /// [boolean] isEnabled
+  static void setEnabled(bool isEnabled) async {
+    final List<dynamic> params = <dynamic>[isEnabled];
+    await _channel.invokeMethod<Object>('setBugReportingEnabled:', params); 
+  } 
+
+   /// Sets a block of code to be executed just before the SDK's UI is presented.
+   /// This block is executed on the UI thread. Could be used for performing any
+   /// UI changes before the SDK's UI is shown.
+   /// [function]  A callback that gets executed before invoking the SDK
+  static void setOnInvokeCallback(Function function) async {
+     _channel.setMethodCallHandler(_handleMethod);
+    onInvokeCallback = function;
+    await _channel.invokeMethod<Object>('setOnInvokeCallback'); 
+  } 
+  
+   /// Sets a block of code to be executed just before the SDK's UI is presented.
+   /// This block is executed on the UI thread. Could be used for performing any
+   /// UI changes before the SDK's UI is shown.
+   /// [function]  A callback that gets executed before invoking the SDK
+  static void setOnDismissCallback(Function function) async {
+     _channel.setMethodCallHandler(_handleMethod);
+    onDismissCallback = function;
+    await _channel.invokeMethod<Object>('setOnDismissCallback'); 
+  } 
 }
