95cb093056e66eeb050639cf0a3606ad6971aa94
Aly ezz <alyezz94@gmail.com>
2019-04-14 13:32:17 +0200

    ✨ Flutter API Mappings

---
+++ lib/Chats.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:instabug_flutter/Instabug.dart';
+
+
+class Chats {
+
+  static const MethodChannel _channel = MethodChannel('instabug_flutter');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  ///Manual invocation for chats view. 
+  static void show() async {
+    await _channel.invokeMethod<Object>('showChats'); 
+  } 
+
+  /// Enables and disables everything related to creating new chats.
+  /// [boolean] isEnabled
+  static void setEnabled(bool isEnabled) async {
+    final List<dynamic> params = <dynamic>[isEnabled];
+    await _channel.invokeMethod<Object>('setChatsEnabled:', params); 
+  } 
+
+}
