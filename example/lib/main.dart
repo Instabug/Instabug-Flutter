d99c5fed01b9fa4b9f1374b2aef9ee0f8f349d54
Aly ezz <alyezz94@gmail.com>
2019-08-06 13:14:09 +0200

    📝 Unify Initialization

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,6 +21,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+     Instabug.start('YOUR_TOKEN', <InvocationEvent>[InvocationEvent.floatingButton]);
     initPlatformState();
   }
 
@@ -28,13 +29,6 @@ class _MyAppState extends State<MyApp> {
   Future<void> initPlatformState() async {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      if (Platform.isIOS) {
-        Instabug.start('YOUR_TOKEN', <InvocationEvent>[InvocationEvent.shake]);
-      }
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
