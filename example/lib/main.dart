f3c5fefc58fc2ff58b2f03c416345b1cf3c0a7b4
Aly ezz <alyezz94@gmail.com>
2019-08-19 13:46:39 +0200

    📝changing sample app invocation mode

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,7 +30,7 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       if (Platform.isIOS) {
-        Instabug.start('YOUR_TOKEN', <InvocationEvent>[InvocationEvent.shake]);
+        Instabug.start('YOUR_TOKEN', <InvocationEvent>[InvocationEvent.floatingButton]);
       }
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
