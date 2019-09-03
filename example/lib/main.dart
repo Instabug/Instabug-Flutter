7a843da030e8212b2e0a3bbeea1990b2e6bcb2f6
Aly ezz <alyezz94@gmail.com>
2019-09-03 13:48:01 +0200

    📝 add sample app token

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-     Instabug.start('YOUR_TOKEN', <InvocationEvent>[InvocationEvent.floatingButton]);
+     Instabug.start('efa41f402620b5654f2af2b86e387029', <InvocationEvent>[InvocationEvent.floatingButton]);
     initPlatformState();
   }
 
