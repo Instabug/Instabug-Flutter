6d754d0ded454e1426f107561b36171a0d460239
Aly ezz <alyezz94@gmail.com>
2020-06-22 10:19:43 +0200

    Handling fast screen transitions

--- lib/InstabugNavigatorObserver.dart
+++ lib/InstabugNavigatorObserver.dart
@@ -1,19 +1,36 @@
-
 import 'package:flutter/material.dart';
 import 'package:instabug_flutter/Instabug.dart';
 
 class InstabugNavigatorObserver extends NavigatorObserver {
 
+  final List<Route> _steps = <Route>[];
+  
+  void screenChanged(final Route newRoute) {
+    // If there is a step that hasn't been pushed yet
+    if (_steps.isNotEmpty) {
+        // Report the last step and remove it from the list
+        Instabug.reportScreenChange(_steps[_steps.length-1].settings.name.toString());
+       _steps.remove(_steps[_steps.length-1]);
+    }
+    // Add the new step to the list
+    _steps.add(newRoute);
+    Future<dynamic>.delayed(const Duration(milliseconds: 1000), () {
+      // If this route is in the array, report it and remove it from the list
+      if (_steps.contains(newRoute)) {
+          Instabug.reportScreenChange(newRoute.settings.name.toString());
+          _steps.remove(newRoute);
+      }   
+    });
+  }
+
   @override
   void didPop(Route route, Route previousRoute) {
-    Instabug.reportScreenChange(previousRoute.settings.name.toString());
-    print(previousRoute.settings.runtimeType.toString());
+    screenChanged(previousRoute);
   }
 
   @override
   void didPush(Route route, Route previousRoute) {
-    Instabug.reportScreenChange(route.settings.name.toString());
-    print(route.settings.name);
+    screenChanged(route);
   }
 
 }
