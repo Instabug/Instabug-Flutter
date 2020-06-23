617ff2699081ac44516851b882fcfe4ff8e3ff3b
Aly ezz <alyezz94@gmail.com>
2020-06-23 09:59:00 +0200

    Add try catch for race condition

--- lib/InstabugNavigatorObserver.dart
+++ lib/InstabugNavigatorObserver.dart
@@ -2,25 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:instabug_flutter/Instabug.dart';
 
 class InstabugNavigatorObserver extends NavigatorObserver {
-
   final List<Route> _steps = <Route>[];
-  
+
   void screenChanged(final Route newRoute) {
-    // If there is a step that hasn't been pushed yet
-    if (_steps.isNotEmpty) {
+    try {
+      // If there is a step that hasn't been pushed yet
+      if (_steps.isNotEmpty) {
         // Report the last step and remove it from the list
-        Instabug.reportScreenChange(_steps[_steps.length-1].settings.name.toString());
-       _steps.remove(_steps[_steps.length-1]);
-    }
-    // Add the new step to the list
-    _steps.add(newRoute);
-    Future<dynamic>.delayed(const Duration(milliseconds: 1000), () {
-      // If this route is in the array, report it and remove it from the list
-      if (_steps.contains(newRoute)) {
+        Instabug.reportScreenChange(
+            _steps[_steps.length - 1].settings.name.toString());
+        _steps.remove(_steps[_steps.length - 1]);
+      }
+      // Add the new step to the list
+      _steps.add(newRoute);
+      Future<dynamic>.delayed(const Duration(milliseconds: 1000), () {
+        // If this route is in the array, report it and remove it from the list
+        if (_steps.contains(newRoute)) {
           Instabug.reportScreenChange(newRoute.settings.name.toString());
           _steps.remove(newRoute);
-      }   
-    });
+        }
+      });
+    } catch (e) {}
   }
 
   @override
@@ -32,5 +34,4 @@ class InstabugNavigatorObserver extends NavigatorObserver {
   void didPush(Route route, Route previousRoute) {
     screenChanged(route);
   }
-
 }
