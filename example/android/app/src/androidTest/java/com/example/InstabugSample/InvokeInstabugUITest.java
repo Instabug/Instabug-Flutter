679a9ab1103b66f198ff85fa0057f3fd667bad26
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-09-26 16:13:05 +0200

    Close media projection dialog in Android UI Tests
    I had to add UiAutomator for this as Espresso can't interact with system UI while UiAutomator does.
    I also bumped the `minSdkVersion` to 18 as UiAutomator's `minSdkVersion` is 18 and we can't make it lower than one of our dependencies.

--- example/android/app/src/androidTest/java/com/example/InstabugSample/InvokeInstabugUITest.java
+++ example/android/app/src/androidTest/java/com/example/InstabugSample/InvokeInstabugUITest.java
@@ -2,12 +2,12 @@ package com.example.InstabugSample;
 
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.UiDevice;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -16,9 +16,8 @@ import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.allOf;
-
-import com.instabug.flutter.InstabugFlutterPlugin;
 
 @RunWith(AndroidJUnit4.class)
 public class InvokeInstabugUITest {
@@ -29,10 +28,13 @@ public class InvokeInstabugUITest {
 
     @Test
     public void ensureInstabugInvocati1on() throws InterruptedException {
-        disableScreenShotByMediaProjection();
         Thread.sleep(1000);
         onView(withResourceName("instabug_floating_button")).perform(click());
         Thread.sleep(1000);
+
+        closeMediaProjectionDialog();
+        Thread.sleep(1000);
+
         onView(withText("Report a bug")).perform(click());
         Thread.sleep(1000);
 
@@ -47,8 +49,9 @@ public class InvokeInstabugUITest {
         onView(withResourceName("instabug_success_dialog_container")).perform(click());
     }
 
-    private void disableScreenShotByMediaProjection() {
-        InstabugFlutterPlugin.enableScreenShotByMediaProjection(false);
+    public static void closeMediaProjectionDialog() {
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        device.pressBack();
     }
 
     public static Method getMethod(Class clazz, String methodName, Class... parameterType) {
