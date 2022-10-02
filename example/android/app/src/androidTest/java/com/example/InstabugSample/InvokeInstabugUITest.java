01c97ebe332a5b20174eb4f029e37f2010df5182
Ahmed Mahmoud <68241710+a7med-mahmoud@users.noreply.github.com>
2022-10-02 13:28:13 +0200

    [MOB-9995] Fix and enable Android UI tests (#271)
    * Fix email `EditText` matcher in Android UI tests
    
    * Close media projection dialog in Android UI Tests
    
    I had to add UiAutomator for this as Espresso can't interact with system UI while UiAutomator does.
    I also bumped the `minSdkVersion` to 18 as UiAutomator's `minSdkVersion` is 18 and we can't make it lower than one of our dependencies.
    
    * Enable Android UI tests on CI
    
    * Fix typo in invocation Android UI test method name
    
    * Check for success dialog with UiAutomator

--- example/android/app/src/androidTest/java/com/example/InstabugSample/InvokeInstabugUITest.java
+++ example/android/app/src/androidTest/java/com/example/InstabugSample/InvokeInstabugUITest.java
@@ -2,6 +2,9 @@ package com.example.InstabugSample;
 
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiSelector;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -10,33 +13,45 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
-import com.instabug.flutter.InstabugFlutterPlugin;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class InvokeInstabugUITest {
+    UiDevice device = UiDevice.getInstance(getInstrumentation());
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void ensureInstabugInvocati1on() throws InterruptedException {
-        disableScreenShotByMediaProjection();
+    public void ensureInstabugInvocation() throws InterruptedException {
         Thread.sleep(1000);
         onView(withResourceName("instabug_floating_button")).perform(click());
         Thread.sleep(1000);
+
+        // Dismiss media projection prompt.
+        // This is a temporary solution as we are dropping media projection in a future release.
+        device.pressBack();
+        Thread.sleep(1000);
+
         onView(withText("Report a bug")).perform(click());
         Thread.sleep(1000);
-        onView(withResourceName("instabug_edit_text_email")).perform(replaceText("inst@bug.com"));
-        onView(withResourceName("instabug_bugreporting_send")).perform(click());
-        onView(withResourceName("instabug_success_dialog_container")).perform(click());
-    }
 
-    private void disableScreenShotByMediaProjection() {
-        InstabugFlutterPlugin.enableScreenShotByMediaProjection(false);
+        onView(
+                allOf(
+                        withResourceName("ib_edit_text"),
+                        withParent(withResourceName("instabug_edit_text_email"))
+                )
+        ).perform(replaceText("inst@bug.com"));
+
+        onView(withResourceName("instabug_bugreporting_send")).perform(click());
+
+        UiObject successDialog = device.findObject(new UiSelector().resourceIdMatches(".*/id:instabug_success_dialog_container"));
+        successDialog.waitForExists(3000);
     }
 }
 
