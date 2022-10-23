17a25a4772a06f85ab09135b4905731716ba7601
Youssef Raafat <yraafat@instabug.com>
2022-10-23 17:40:46 +0200

    Add NetworkData Unit Tests

--- test/instabug_flutter_test.dart
+++ test/instabug_flutter_test.dart
@@ -29,31 +29,6 @@ void main() {
   const userAttributePair = <String, String>{'gender': 'female'};
   late MockIBGBuildInfo mockBuildInfo;
 
-  const url = 'https://jsonplaceholder.typicode.com';
-  const method = 'POST';
-  final startDate = DateTime.now();
-  final endDate = DateTime.now().add(const Duration(hours: 1));
-  const requestBody = 'requestBody';
-  const responseBody = 'responseBody';
-  const status = 200;
-  const requestHeaders = <String, dynamic>{'request': 'request'};
-  const responseHeaders = <String, dynamic>{'response': 'response'};
-  const duration = 10;
-  const contentType = 'contentType';
-  final networkData = NetworkData(
-    url: url,
-    method: method,
-    startTime: startDate,
-    requestContentType: contentType,
-    responseContentType: contentType,
-    duration: duration,
-    endTime: endDate,
-    requestBody: requestBody,
-    responseBody: responseBody,
-    requestHeaders: requestHeaders,
-    responseHeaders: responseHeaders,
-    status: status,
-  );
 
   setUpAll(() async {
     const MethodChannel('instabug_flutter')
@@ -101,75 +76,7 @@ void main() {
   });
 
 
-  test('Test NetworkData model ToMap', () async {
-    final newNetworkData = networkData.toMap();
-    expect(networkData.url, newNetworkData['url']);
-    expect(networkData.method, newNetworkData['method']);
-    expect(
-      networkData.requestContentType,
-      newNetworkData['requestContentType'],
-    );
-    expect(
-      networkData.responseContentType,
-      newNetworkData['responseContentType'],
-    );
-    expect(networkData.duration, newNetworkData['duration']);
-    expect(networkData.requestBody, newNetworkData['requestBody']);
-    expect(networkData.responseBody, newNetworkData['responseBody']);
-    expect(networkData.requestHeaders, newNetworkData['requestHeaders']);
-    expect(networkData.responseHeaders, newNetworkData['responseHeaders']);
-  });
-  test('Test NetworkData model CopyWith empty', () async {
-    final newNetworkData = networkData.copyWith();
-    final newNetworkDataMap = newNetworkData.toMap();
-    final networkDataMap = networkData.toMap();
-    networkDataMap.forEach((key, dynamic value) {
-      expect(value, newNetworkDataMap[key]);
-    });
-  });
 
-  test('Test NetworkData model CopyWith', () async {
-    const urlCopy = 'https://jsonplaceholder.typicode.comCopy';
-    const methodCopy = 'POSTCopy';
-    const requestBodyCopy = 'requestBodyCopy';
-    const responseBodyCopy = 'responseBodyCopy';
-    const requestHeadersCopy = <String, dynamic>{'requestCopy': 'requestCopy'};
-    const responseHeadersCopy = <String, dynamic>{
-      'responseCopy': 'responseCopy'
-    };
-    const durationCopy = 20;
-    const contentTypeCopy = 'contentTypeCopy';
-    final startDateCopy = DateTime.now().add(const Duration(days: 1));
-    final endDateCopy = DateTime.now().add(const Duration(days: 2));
-    const statusCopy = 300;
 
-    final newNetworkData = networkData.copyWith(
-      url: urlCopy,
-      method: methodCopy,
-      requestBody: requestBodyCopy,
-      requestHeaders: requestHeadersCopy,
-      responseBody: responseBodyCopy,
-      responseHeaders: responseHeadersCopy,
-      duration: durationCopy,
-      requestContentType: contentTypeCopy,
-      responseContentType: contentTypeCopy,
-      startTime: startDateCopy,
-      endTime: endDateCopy,
-      status: statusCopy,
-    );
-
-    expect(newNetworkData.url, urlCopy);
-    expect(newNetworkData.method, methodCopy);
-    expect(newNetworkData.requestBody, requestBodyCopy);
-    expect(newNetworkData.requestHeaders, requestHeadersCopy);
-    expect(newNetworkData.responseBody, responseBodyCopy);
-    expect(newNetworkData.responseHeaders, responseHeadersCopy);
-    expect(newNetworkData.duration, durationCopy);
-    expect(newNetworkData.requestContentType, contentTypeCopy);
-    expect(newNetworkData.responseContentType, contentTypeCopy);
-    expect(newNetworkData.startTime, startDateCopy);
-    expect(newNetworkData.endTime, endDateCopy);
-    expect(newNetworkData.status, statusCopy);
-  });
 
 }
