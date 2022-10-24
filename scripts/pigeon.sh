59e4502e9590120f8d662f62e664f27feb7fb86e
Youssef Raafat <yraafat@instabug.com>
2022-10-24 08:12:29 +0200

    Clean pigeon script

--- scripts/pigeon.sh
+++ scripts/pigeon.sh
@@ -1,67 +1,34 @@
-mkdir -p lib/generated
-mkdir -p android/src/main/java/com/instabug/flutter/generated
-mkdir -p ios/Classes/Generated
+#!/bin/bash
 
-flutter pub run pigeon \
-  --input pigeons/apm.api.dart \
-  --dart_out lib/generated/apm.api.g.dart \
-  --objc_header_out ios/Classes/Generated/ApmPigeon.h \
-  --objc_source_out ios/Classes/Generated/ApmPigeon.m \
-  --java_out ./android/src/main/java/com/instabug/flutter/generated/ApmPigeon.java \
-  --java_package "com.instabug.flutter.generated"
+DIR_DART="lib/generated"
+DIR_IOS="ios/Classes/Generated"
+DIR_ANDROID="android/src/main/java/com/instabug/flutter/generated"
+PKG_ANDROID="com.instabug.flutter.generated"
 
-flutter pub run pigeon \
-  --input pigeons/bug_reporting.api.dart \
-  --dart_out lib/generated/bug_reporting.api.g.dart \
-  --objc_header_out ios/Classes/Generated/BugReportingPigeon.h \
-  --objc_source_out ios/Classes/Generated/BugReportingPigeon.m \
-  --java_out ./android/src/main/java/com/instabug/flutter/generated/BugReportingPigeon.java \
-  --java_package "com.instabug.flutter.generated"
+mkdir -p $DIR_DART
+mkdir -p $DIR_IOS
+mkdir -p $DIR_ANDROID
 
-flutter pub run pigeon \
-  --input pigeons/crash_reporting.api.dart \
-  --dart_out lib/generated/crash_reporting.api.g.dart \
-  --objc_header_out ios/Classes/Generated/CrashReportingPigeon.h \
-  --objc_source_out ios/Classes/Generated/CrashReportingPigeon.m \
-  --java_out ./android/src/main/java/com/instabug/flutter/generated/CrashReportingPigeon.java \
-  --java_package "com.instabug.flutter.generated"
+generate_pigeon() {
+  name_snake=$1
+  name_pascal=$(echo "$name_snake" | perl -pe 's/(^|_)./uc($&)/ge;s/_//g')
 
-flutter pub run pigeon \
-  --input pigeons/feature_requests.api.dart \
-  --dart_out lib/generated/feature_requests.api.g.dart \
-  --objc_header_out ios/Classes/Generated/FeatureRequestsPigeon.h \
-  --objc_source_out ios/Classes/Generated/FeatureRequestsPigeon.m \
-  --java_out ./android/src/main/java/com/instabug/flutter/generated/FeatureRequestsPigeon.java \
-  --java_package "com.instabug.flutter.generated"
+  flutter pub run pigeon \
+  --input "pigeons/$name_snake.api.dart" \
+  --dart_out "$DIR_DART/$name_snake.api.g.dart" \
+  --objc_header_out "$DIR_IOS/${name_pascal}Pigeon.h" \
+  --objc_source_out "$DIR_IOS/${name_pascal}Pigeon.m" \
+  --java_out "$DIR_ANDROID/${name_pascal}Pigeon.java" \
+  --java_package $PKG_ANDROID || exit 1;
 
-flutter pub run pigeon \
-  --input pigeons/instabug.api.dart \
-  --dart_out lib/generated/instabug.api.g.dart \
-  --objc_header_out ios/Classes/Generated/InstabugPigeon.h \
-  --objc_source_out ios/Classes/Generated/InstabugPigeon.m \
-  --java_out ./android/src/main/java/com/instabug/flutter/generated/InstabugPigeon.java \
-  --java_package "com.instabug.flutter.generated"
+  echo "Generated $name_snake pigeon"
+}
 
-flutter pub run pigeon \
-  --input pigeons/instabug_log.api.dart \
-  --dart_out lib/generated/instabug_log.api.g.dart \
-  --objc_header_out ios/Classes/Generated/InstabugLogPigeon.h \
-  --objc_source_out ios/Classes/Generated/InstabugLogPigeon.m \
-  --java_out ./android/src/main/java/com/instabug/flutter/generated/InstabugLogPigeon.java \
-  --java_package "com.instabug.flutter.generated"
-
-flutter pub run pigeon \
-  --input pigeons/replies.api.dart \
-  --dart_out lib/generated/replies.api.g.dart \
-  --objc_header_out ios/Classes/Generated/RepliesPigeon.h \
-  --objc_source_out ios/Classes/Generated/RepliesPigeon.m \
-  --java_out ./android/src/main/java/com/instabug/flutter/generated/RepliesPigeon.java \
-  --java_package "com.instabug.flutter.generated"
-
-flutter pub run pigeon \
-  --input pigeons/surveys.api.dart \
-  --dart_out lib/generated/surveys.api.g.dart \
-  --objc_header_out ios/Classes/Generated/SurveysPigeon.h \
-  --objc_source_out ios/Classes/Generated/SurveysPigeon.m \
-  --java_out ./android/src/main/java/com/instabug/flutter/generated/SurveysPigeon.java \
-  --java_package "com.instabug.flutter.generated"
+generate_pigeon apm
+generate_pigeon bug_reporting
+generate_pigeon crash_reporting
+generate_pigeon feature_requests
+generate_pigeon instabug
+generate_pigeon instabug_log
+generate_pigeon replies
+generate_pigeon surveys
