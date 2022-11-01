bd23c0227ddd88ba67cf0cda068a69352a887d80
Youssef Raafat <yraafat@instabug.com>
2022-11-01 12:23:46 +0200

    Run Pigeon Script For All API Files

--- scripts/pigeon.sh
+++ scripts/pigeon.sh
@@ -10,16 +10,17 @@ mkdir -p $DIR_IOS
 mkdir -p $DIR_ANDROID
 
 generate_pigeon() {
-  name_snake=$1
+  name_file=$1
+  name_snake=$(basename $name_file .api.dart)
   name_pascal=$(echo "$name_snake" | perl -pe 's/(^|_)./uc($&)/ge;s/_//g')
 
   flutter pub run pigeon \
-  --input "pigeons/$name_snake.api.dart" \
-  --dart_out "$DIR_DART/$name_snake.api.g.dart" \
-  --objc_header_out "$DIR_IOS/${name_pascal}Pigeon.h" \
-  --objc_source_out "$DIR_IOS/${name_pascal}Pigeon.m" \
-  --java_out "$DIR_ANDROID/${name_pascal}Pigeon.java" \
-  --java_package $PKG_ANDROID || exit 1;
+      --input "pigeons/$name_snake.api.dart" \
+      --dart_out "$DIR_DART/$name_snake.api.g.dart" \
+      --objc_header_out "$DIR_IOS/${name_pascal}Pigeon.h" \
+      --objc_source_out "$DIR_IOS/${name_pascal}Pigeon.m" \
+      --java_out "$DIR_ANDROID/${name_pascal}Pigeon.java" \
+      --java_package $PKG_ANDROID || exit 1;
 
   echo "Generated $name_snake pigeon"
 
@@ -28,11 +29,7 @@ generate_pigeon() {
   flutter format "$DIR_DART/$name_snake.api.g.dart"
 }
 
-generate_pigeon apm
-generate_pigeon bug_reporting
-generate_pigeon crash_reporting
-generate_pigeon feature_requests
-generate_pigeon instabug
-generate_pigeon instabug_log
-generate_pigeon replies
-generate_pigeon surveys
+for file in pigeons/**
+do
+  generate_pigeon $file
+done
