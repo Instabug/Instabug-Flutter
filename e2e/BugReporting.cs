80c9b877498c523d1c0b24ccf739b4017fd1e91a
Ahmed Mahmoud <68241710+a7medev@users.noreply.github.com>
2022-11-09 14:41:06 +0200

    [MOB-10374] Add E2E tests with Captain (#285)
    * Remove native Android and iOS E2E tests
    
    * Add bug reporting E2E tests with Captain
    
    * Migrate CI to Appium and Captain
    
    * Fix .NET install in `test_android` job
    
    * Clone Captain using SSH on CI
    
    * Fix work dir for Android build and test commands
    
    * Fix E2E Android APK
    
    * Fix Android E2E test working directory
    
    * Separate unit test jobs from E2E test ones
    
    * Revert "Remove native Android and iOS E2E tests"
    
    This reverts commit 6fd77e2341ae5cf7cc27dba4215916efca43b7be.
    
    * Return Espresso & XCUITest tests
    
    * Upscale `e2e_ios_captain` macOS machine to large
    
    * Update E2E dependencies
    
    * Move E2E tests to root directory

---
+++ e2e/BugReporting.cs
@@ -0,0 +1,39 @@
+using Xunit;
+using Instabug.Captain;
+
+namespace E2E;
+
+public class BugReporting
+{
+  readonly Captain captain = new(
+      androidApp: Path.GetFullPath("../../../../example/build/app/outputs/flutter-apk/app-debug.apk"),
+      androidVersion: "11",
+      iosApp: Path.GetFullPath("../../../../example/build/ios/iphonesimulator/Runner.app"),
+      iosVersion: "15.5"
+  );
+
+  [Fact]
+  public void ReportABug()
+  {
+    captain.FindById(
+        android: "instabug_floating_button",
+        ios: "IBGFloatingButtonAccessibilityIdentifier"
+    ).Tap();
+
+    if (captain.IsAndroid) captain.GoBack();
+
+    captain.FindByText("Report a bug").Tap();
+
+    captain.FindInput(
+        android: "instabug_edit_text_email",
+        ios: "IBGBugInputViewEmailFieldAccessibilityIdentifier"
+    ).Type("inst@bug.com");
+
+    captain.FindById(
+        android: "instabug_bugreporting_send",
+        ios: "IBGBugVCNextButtonAccessibilityIdentifier"
+    ).Tap();
+
+    Assert.True(captain.FindByText("Thank you").Displayed);
+  }
+}
