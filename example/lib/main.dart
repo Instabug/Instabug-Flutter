fda575e82daf1d324493200ec048799d7c1f04a9
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-12-07 12:33:35 +0200

    Refactor example app with reusable widgits

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,7 +15,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,19 +23,7 @@ class MyApp extends StatelessWidget {
         InstabugNavigatorObserver(),
       ],
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -44,17 +31,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class InstabugButton extends StatelessWidget {
+  String text;
+  void Function()? onPressed;
+
+  InstabugButton({required this.text, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
+
+class SectionTitle extends StatelessWidget {
+  String text;
+
+  SectionTitle(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: const EdgeInsets.only(top: 20.0, left: 20.0),
+      child: Text(
+        text,
+        textAlign: TextAlign.left,
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -63,6 +83,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final buttonStyle = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
+    foregroundColor: MaterialStateProperty.all(Colors.white),
+  );
+
   void show() {
     Instabug.show();
   }
@@ -107,23 +132,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final buttonStyle = ButtonStyle(
-      backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
-      foregroundColor: MaterialStateProperty.all(Colors.white),
-    );
-
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: SingleChildScrollView(
           padding: const EdgeInsets.only(top: 20.0),
           child: Column(
@@ -137,85 +147,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   textAlign: TextAlign.center,
                 ),
               ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                // height: double.infinity,
-                child: ElevatedButton(
-                  onPressed: show,
-                  style: buttonStyle,
-                  child: const Text('Invoke'),
-                ),
+              InstabugButton(
+                onPressed: sendBugReport,
+                text: 'Send Bug Report',
               ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                // height: double.infinity,
-                child: ElevatedButton(
-                  onPressed: sendBugReport,
-                  style: buttonStyle,
-                  child: const Text('Send Bug Report'),
-                ),
+              InstabugButton(
+                onPressed: sendFeedback,
+                text: 'Send Feedback',
               ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                // height: double.infinity,
-                child: ElevatedButton(
-                  onPressed: sendFeedback,
-                  style: buttonStyle,
-                  child: const Text('Send Feedback'),
-                ),
+              InstabugButton(
+                onPressed: askQuestion,
+                text: 'Ask a Question',
               ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                // height: double.infinity,
-                child: ElevatedButton(
-                  onPressed: askQuestion,
-                  style: buttonStyle,
-                  child: const Text('Ask a Question'),
-                ),
+              InstabugButton(
+                onPressed: showNpsSurvey,
+                text: 'Show NPS Survey',
               ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                // height: double.infinity,
-                child: ElevatedButton(
-                  onPressed: showNpsSurvey,
-                  style: buttonStyle,
-                  child: const Text('Show NPS Survey'),
-                ),
+              InstabugButton(
+                onPressed: showMultipleQuestionSurvey,
+                text: 'Show Multiple Questions Survey',
               ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                // height: double.infinity,
-                child: ElevatedButton(
-                  onPressed: showMultipleQuestionSurvey,
-                  style: buttonStyle,
-                  child: const Text('Show Multiple Questions Survey'),
-                ),
+              InstabugButton(
+                onPressed: showFeatureRequests,
+                text: 'Show Feature Requests',
               ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                // height: double.infinity,
-                child: ElevatedButton(
-                  onPressed: showFeatureRequests,
-                  style: buttonStyle,
-                  child: const Text('Show Feature Requests'),
-                ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                margin: const EdgeInsets.only(top: 20.0, left: 20.0),
-                child: const Text(
-                  'Change Invocation Event',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
+              SectionTitle('Change Invocation Event'),
               ButtonBar(
                 mainAxisSize: MainAxisSize.min,
                 alignment: MainAxisAlignment.start,
@@ -256,15 +212,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
-              Container(
-                alignment: Alignment.centerLeft,
-                margin: const EdgeInsets.only(left: 20.0),
-                child: const Text(
-                  'Set Primary Color',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
+              SectionTitle('Set Primary Color'),
               ButtonBar(
                 mainAxisSize: MainAxisSize.min,
                 alignment: MainAxisAlignment.center,
@@ -325,15 +273,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
-              Container(
-                alignment: Alignment.centerLeft,
-                margin: const EdgeInsets.only(left: 20.0),
-                child: const Text(
-                  'Color Theme',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
+              SectionTitle('Color Theme'),
               ButtonBar(
                 mainAxisSize: MainAxisSize.max,
                 alignment: MainAxisAlignment.center,
