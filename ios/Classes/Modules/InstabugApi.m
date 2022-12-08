3609f17e5b950944f26dd040876cabe1021f4c45
David Mina <david.mina96@hotmail.com>
2022-12-08 15:12:37 +0200

    Add a check for string key existence on iOS

--- ios/Classes/Modules/InstabugApi.m
+++ ios/Classes/Modules/InstabugApi.m
@@ -90,8 +90,10 @@ extern void InitInstabugApi(id<FlutterBinaryMessenger> messenger) {
 }
 
 - (void)setValueForStringWithKeyValue:(NSString *)value key:(NSString *)key error:(FlutterError *_Nullable *_Nonnull)error {
-    NSString *resolvedKey = ArgsRegistry.placeholders[key];
-    [Instabug setValue:value forStringWithKey:resolvedKey];
+    if ([ArgsRegistry.placeholders objectForKey:key]) {
+        NSString *resolvedKey = ArgsRegistry.placeholders[key];
+        [Instabug setValue:value forStringWithKey:resolvedKey];
+    }
 }
 
 - (void)appendTagsTags:(NSArray<NSString *> *)tags error:(FlutterError *_Nullable *_Nonnull)error {
