087a4cf0ad7789c67d3325eff386b4cc3438e8bd
Ahmed Mahmoud <68241710+a7medev@users.noreply.github.com>
2022-12-22 07:43:26 +0200

    [MOB-10730] Add More Android E2E Tests (#311)
    * Refactor example app with reusable widgits
    
    * Setup Espresso for Flutter
    
    * UI test Android primary color changes
    
    * UI test Android invocation events
    
    * UI test Android repro steps
    
    * UI test Android manual surveys
    
    * UI test Android bug reporting onDismiss callback
    
    * Improve Android UI test stability
    
    * UI test Android report types
    
    * UI test Android floating button edge
    
    * Fix Android repro steps UI test
    
    * Improve report types Android UI test stability
    
    * UI test Android feature requests
    
    * Wait for options prompt in Android UI tests
    
    * Wait for elements in Android UI tests
    
    * Unify Espresso matchers usage in UI tests
    
    * Improve code quality

---
+++ example/android/app/src/androidTest/java/com/example/InstabugSample/SurveysUITest.java
@@ -0,0 +1,30 @@
+package com.example.InstabugSample;
+
+import static androidx.test.espresso.flutter.EspressoFlutter.onFlutterWidget;
+
+import static com.example.InstabugSample.util.InstabugAssertions.assertViewWillBeVisible;
+
+import androidx.test.espresso.flutter.action.FlutterActions;
+import androidx.test.espresso.flutter.matcher.FlutterMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class SurveysUITest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void showManualSurvey() {
+        onFlutterWidget(FlutterMatchers.withText("Show Manual Survey"))
+                .perform(FlutterActions.scrollTo(), FlutterActions.click());
+
+        assertViewWillBeVisible("instabug_survey_dialog_container", 2000);
+    }
+}
