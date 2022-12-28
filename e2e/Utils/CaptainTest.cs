4f225058eee8b9e4283d419af93c3b473f0cfe56
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-12-29 01:07:08 +0200

    Share Captain between all tests

--- e2e/Utils/CaptainTest.cs
+++ e2e/Utils/CaptainTest.cs
@@ -3,7 +3,7 @@ using Instabug.Captain;
 
 namespace E2E.Utils;
 
-public class CaptainTest
+public class CaptainTest : IDisposable
 {
   private static readonly CaptainConfig _config = new()
   {
@@ -12,12 +12,17 @@ public class CaptainTest
     IosApp = Path.GetFullPath("../../../../example/build/ios/iphonesimulator/Runner.app"),
     IosVersion = "15.5"
   };
-  protected readonly Captain captain = new(_config);
+  protected static readonly Captain captain = new(_config);
 
   public CaptainTest()
   {
     // Wait till the app is ready
     captain.FindByText("Hello Instabug");
+  }
+
+  public void Dispose()
+  {
+    captain.ResetApp();
   }
 
   protected void ScrollDown()
