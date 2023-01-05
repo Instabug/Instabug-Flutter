eab6bd32a759f2e71c449d4e81a59b66b42268b7
Ahmed Mahmoud <68241710+a7medev@users.noreply.github.com>
2023-01-05 17:59:39 +0200

    [MOB-10607] Add More Captain E2E Tests (#320)

---
+++ e2e/FeatureRequestsTests.cs
@@ -0,0 +1,22 @@
+﻿using E2E.Utils;
+using Instabug.Captain;
+using Xunit;
+
+namespace E2E;
+
+[Collection("E2E")]
+public class FeatureRequestsTests : CaptainTest
+{
+  [Fact]
+  public void ShowFeatureRequetsScreen()
+  {
+    ScrollDown();
+    captain.FindByText("Show Feature Requests").Tap();
+
+    var screenTitle = captain.FindById(
+        android: "ib_fr_toolbar_main",
+        ios: "IBGFeatureListTableView"
+    );
+    Assert.True(screenTitle.Displayed);
+  }
+}
