2bfd7d70091f4c2c45a2ef393e51cb1f1d8bc333
David Mina <david.mina96@hotmail.com>
2023-02-19 13:57:11 +0200

    Remove existing example app

--- example/ios/InstabugSampleTests/Util/IBGSurvey+Test.h
+++
@@ -1,7 +0,0 @@
-// This header file makes IBGSurvey title property editable to be used in tests.
-
-#import <Instabug/IBGSurveys.h>
-
-@interface IBGSurvey (Test)
-@property (nonatomic, strong) NSString *title;
-@end
