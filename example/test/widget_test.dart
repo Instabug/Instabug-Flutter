4ee5b576714f5eb7e52cf739960ee68d717d317d
David Mina <david.mina96@hotmail.com>
2023-02-20 14:09:18 +0200

    Rename Example

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:instabug_sample/main.dart';
+import 'package:instabug_flutter_example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
--- example/test_driver/example.dart
+++ example/test_driver/example.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:instabug_sample/main.dart' as app;
+import 'package:instabug_flutter_example/main.dart' as app;
 
 void main() {
   enableFlutterDriverExtension();
