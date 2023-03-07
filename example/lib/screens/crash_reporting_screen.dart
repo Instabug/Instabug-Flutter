d9a9ab81a2b6d850e4b29935fa1dd0cd8f6ff9c8
David Mina <david.mina96@hotmail.com>
2023-03-07 17:23:24 +0200

    Add Crash Reporting screen

---
+++ example/lib/screens/crash_reporting_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:instabug_flutter/instabug_flutter.dart';
+
+import '../widgets/feature_tile.dart';
+import '../widgets/section_card.dart';
+import '../widgets/separated_list_view.dart';
+
+class CrashReportingScreen extends StatefulWidget {
+  const CrashReportingScreen({super.key});
+
+  @override
+  State<CrashReportingScreen> createState() => _CrashReportingScreenState();
+}
+
+class _CrashReportingScreenState extends State<CrashReportingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Crash Reporting',
+        ),
+      ),
+      body: MediaQuery.removePadding(
+        context: context,
+        removeBottom: true,
+        removeTop: true,
+        child: SeparatedListView(
+          padding: const EdgeInsets.all(8.0),
+          separator: const SizedBox(
+            height: 8.0,
+          ),
+          children: [
+            SectionCard(
+              children: [
+                FeatureTile(
+                  leading: const Icon(Icons.warning_amber_outlined),
+                  title: const Text('Handled Crash'),
+                  onTap: () {
+                    try {
+                      throw Exception(
+                        'This is a handled crash from Instabug Example App',
+                      );
+                    } catch (e, st) {
+                      CrashReporting.reportHandledCrash(e, st);
+                      const snackBar = SnackBar(
+                        content: Text(
+                          'A handled crash has been successfully reported!',
+                        ),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                  },
+                ),
+                FeatureTile(
+                  leading: const Icon(Icons.warning),
+                  title: const Text('Unhandled Crash'),
+                  onTap: () {
+                    try {
+                      final arr = [1, 2];
+                      arr[2];
+                    } finally {
+                      const snackBarText = kDebugMode
+                          ? 'Unhandled Crashes will only be reported in release mode and not in debug mode.'
+                          : 'An unhandled crash has been successfully reported!';
+                      const snackBar = SnackBar(
+                        content: Text(snackBarText),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
