5148dfbd37b38976eafe72c8a4c7b853214221ac
David Mina <david.mina96@hotmail.com>
2023-03-07 17:23:33 +0200

    Add APM screen

---
+++ example/lib/screens/apm_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class APMScreen extends StatelessWidget {
+  const APMScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'APM',
+        ),
+      ),
+    );
+  }
+}
