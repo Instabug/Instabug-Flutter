5efc40eda4f2afad2c465e0b0661bf8e096e4ae9
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-04-10 11:53:59 +0200

    Improve the checks for `lib` and PR description

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,17 +1,19 @@
 import { danger, fail, schedule, warn } from 'danger';
 
-const hasAppChanges = !danger.git.modified_files.some((file) =>
-  file.includes('lib')
+const hasSourceChanges = danger.git.modified_files.some((file) =>
+  file.startsWith('lib/')
 );
 const declaredTrivial =
-  (danger.github.pr.title + danger.github.pr.body).includes('#trivial') ||
-  !hasAppChanges ||
+  !hasSourceChanges ||
   danger.github.issue.labels.some((label) => label.name === 'trivial');
 
 // Make sure PR has a description.
 async function hasDescription() {
   const linesOfCode = (await danger.git.linesOfCode()) ?? 0;
-  if (danger.github.pr.body.length < 3 && linesOfCode > 10) {
+  const hasNoDescription = danger.github.pr.body.includes(
+    '> Description goes here'
+  );
+  if (hasNoDescription && linesOfCode > 10) {
     fail(
       'Please provide a summary of the changes in the pull request description.'
     );
