2fcd8e1798f9b5ca698890505c4c30ab8797092b
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-09-10 15:06:58 +0300

    chore: move obfuscateLog docs to network logger

--- lib/src/utils/network_manager.dart
+++ lib/src/utils/network_manager.dart
@@ -14,24 +14,6 @@ class NetworkManager {
     _obfuscateLogCallback = callback;
   }
 
-  /// Registers a callback to selectively obfuscate network log data.
-  ///
-  /// Use this method to set a callback function that determines whether
-  /// specific network log data should undergo obfuscation before being recorded
-  /// or logged.
-  ///
-  /// The [callback] function takes a [NetworkData] object as its argument and
-  /// should return a modified [NetworkData] object with sensitive information
-  /// obfuscated.
-  ///
-  /// Example:
-  ///
-  /// ```dart
-  /// NetworkLogger.obfuscateLog((data) {
-  ///   // Implement custom logic to obfuscate sensitive data here.
-  ///   // Modify 'data' as needed and return it.
-  /// });
-  /// ```
   FutureOr<NetworkData> obfuscateLog(NetworkData data) {
     if (_obfuscateLogCallback == null) {
       return data;
