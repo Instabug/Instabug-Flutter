6f42a7aae1ad8488e6b81e9eb6a05e31173ae1c1
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-09-27 19:49:56 +0300

    docs: update doc comments on session replay apis

--- lib/src/modules/session_replay.dart
+++ lib/src/modules/session_replay.dart
@@ -17,60 +17,48 @@ class SessionReplay {
 
   /// Enables or disables Session Replay for your Instabug integration.
   ///
-  /// Session Replay allows you to capture user interactions with your app
-  /// for analysis and debugging. By default, Session Replay is enabled if
-  /// it is available in your current plan.
+  /// By default, Session Replay is enabled if it is available in your current plan.
   ///
   /// Example:
   ///
   /// ```dart
-  /// SessionReplay.setEnabled(true); // Enable Session Replay
-  /// SessionReplay.setEnabled(false); // Disable Session Replay
+  /// await SessionReplay.setEnabled(true);
   /// ```
   static Future<void> setEnabled(bool isEnabled) async {
     return _host.setEnabled(isEnabled);
   }
 
   /// Enables or disables network logs for Session Replay.
-  ///
-  /// Network logs include details of network requests and responses made
-  /// during a session. By default, network logs are enabled.
+  /// By default, network logs are enabled.
   ///
   /// Example:
   ///
   /// ```dart
-  /// await SessionReplay.setNetworkLogsEnabled(true); // Enable network logs
-  /// await SessionReplay.setNetworkLogsEnabled(false); // Disable network logs
+  /// await SessionReplay.setNetworkLogsEnabled(true);
   /// ```
   static Future<void> setNetworkLogsEnabled(bool isEnabled) async {
     return _host.setNetworkLogsEnabled(isEnabled);
   }
 
-  /// Enables or disables Instabug internal logs for Session Replay.
-  ///
-  /// Instabug logs include internal debugging information. By default,
-  /// Instabug logs are enabled.
+  /// Enables or disables Instabug logs for Session Replay.
+  /// By default, Instabug logs are enabled.
   ///
   /// Example:
   ///
   /// ```dart
-  /// await SessionReplay.setInstabugLogsEnabled(true); // Enable Instabug logs
-  /// await SessionReplay.setInstabugLogsEnabled(false); // Disable Instabug logs
+  /// await SessionReplay.setInstabugLogsEnabled(true);
   /// ```
   static Future<void> setInstabugLogsEnabled(bool isEnabled) async {
     return _host.setInstabugLogsEnabled(isEnabled);
   }
 
   /// Enables or disables capturing of user steps  for Session Replay.
-  ///
-  /// User steps are a sequence of interactions performed by the user
-  /// within the app. By default, user steps are enabled.
+  /// By default, user steps are enabled.
   ///
   /// Example:
   ///
   /// ```dart
-  /// await SessionReplay.setUserStepsEnabled(true); // Enable user steps
-  /// await SessionReplay.setUserStepsEnabled(false); // Disable user steps
+  /// await SessionReplay.setUserStepsEnabled(true);
   /// ```
   static Future<void> setUserStepsEnabled(bool isEnabled) async {
     return _host.setUserStepsEnabled(isEnabled);
