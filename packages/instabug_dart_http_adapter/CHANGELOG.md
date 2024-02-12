ffd3eedb78d50607af62bd6486dffae6f470bd71
AbdElHamid Nasser <anasser@instabug.com>
2024-02-12 13:25:14 +0200

    refactor: merge instabug_dart_http_adapter

---
+++ packages/instabug_dart_http_adapter/CHANGELOG.md
@@ -0,0 +1,24 @@
+## [2.3.0] - 3/11/2022
+
+- Adds support for MultipartRequest.
+
+## [2.2.1] - 2/8/2022
+
+- Bumps [instabug_flutter](https://pub.dev/packages/instabug_flutter) to v11
+
+## [2.2.0] - 11/4/2022
+
+- Adds support for logging network requests using `send` method.
+
+## [2.1.0] - 5/1/2022
+
+- Fixes network log compilation error.
+- Adds payload size for network log.
+
+## [2.0.0] - 30/11/2021
+
+- Upgrades to null safety.
+
+## [1.0.0] - 29/7/2019
+
+- Adds implementation for the instabug_http_client library which supports Instabug network logging for the dart library: http.
---
+++ packages/instabug_dart_http_adapter/example/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:instabug_http_client/instabug_http_client.dart';
+import 'package:instabug_flutter/instabug_flutter.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+  Instabug.start(
+      'ed6f659591566da19b67857e1b9d40ab', [InvocationEvent.floatingButton]);
+  final client = InstabugHttpClient();
+  final response = await client.get(Uri.parse('https://google.com'));
+  print(response.body);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
