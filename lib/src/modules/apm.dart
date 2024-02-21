9c093658f84c8c09287f91f6fb6626ae9fe905d7
AbdElHamid Nasser <anasser@instabug.com>
2024-02-21 12:09:00 +0200

    chore: reformat files

--- lib/src/modules/apm.dart
+++ lib/src/modules/apm.dart
@@ -112,7 +112,8 @@ class APM {
   ///
   /// To remove an attribute, set its [value] to null. Attributes cannot be added or
   /// modified after an AppFlow has concluded.
-  static Future<void> setFlowAttribute(String name, String key, String? value) async {
+  static Future<void> setFlowAttribute(
+      String name, String key, String? value) async {
     return _host.setFlowAttribute(name, key, value);
   }
 
