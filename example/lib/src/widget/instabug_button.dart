7e0312094f6d9175dd262f58795f427a46b4454f
AbdElHamid Nasser <anasser@instabug.com>
2024-03-03 14:32:19 +0200

    feat(example): add smallFontSize button constructor
    Introduces a new constructor to the InstabugButton widget, to allow creation of create buttons with a smaller font size. The new constructor, InstabugButton.smallFontSize, accepts parameters for text, onPressed callback, fontSize (defaulting to 10.0), and margin. This addition enhances the flexibility of the InstabugButton widget.

--- example/lib/src/widget/instabug_button.dart
+++ example/lib/src/widget/instabug_button.dart
@@ -4,23 +4,38 @@ class InstabugButton extends StatelessWidget {
   const InstabugButton({Key? key,
     required this.text,
     this.onPressed,
+    this.fontSize,
+    this.margin,
+  }) : super(key: key);
+
+  const InstabugButton.smallFontSize({
+    Key? key,
+    required this.text,
+    this.onPressed,
+    this.fontSize = 10.0,
+    this.margin,
   }) : super(key: key);
 
   final String text;
   final Function()? onPressed;
+  final double? fontSize;
+
+  final EdgeInsetsGeometry? margin;
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      margin: const EdgeInsets.symmetric(
+      margin: margin ?? const EdgeInsets.symmetric(
         horizontal: 20.0,
       ),
       child: ElevatedButton(
         onPressed: onPressed,
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
-          foregroundColor: MaterialStateProperty.all(Colors.white),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.lightBlue,
+          foregroundColor: Colors.white,
+          textStyle: Theme.of(context).textTheme.labelLarge?.copyWith(fontSize: fontSize),
         ),
         child: Text(text),
       ),
