60156b130ca6cdbd10d0859d5cb24c4f7bd0c3e5
Ahmed Mahmoud <68241710+a7medev@users.noreply.github.com>
2024-07-07 11:39:07 +0300

    test: decommission native e2e tests in favor of captain (#484)

--- example/ios/InstabugUITests/Util/InstabugUITestsUtils.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-@interface InstabugUITestsUtils : NSObject
-
-+ (UIColor *)getPixelColorWithImage:(UIImage *)image x:(NSInteger)x y:(NSInteger)y;
-
-@end
-
