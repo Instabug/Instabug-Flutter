2fe95862cc0d34370cb6aac3ec8d21f9b973777e
Ahmed alaa <aalaa@instabug.com>
2024-07-16 21:51:26 +0300

    feat:add w3 header

--- ios/Classes/Modules/InstabugApi.m
+++ ios/Classes/Modules/InstabugApi.m
@@ -5,7 +5,7 @@
 #import "IBGNetworkLogger+CP.h"
 #import "InstabugApi.h"
 #import "ArgsRegistry.h"
-#import "../Util/IBGAPM+PrivateAPIs.h"
+#import "../Util/NativeUtils/IBGAPM+PrivateAPIs.h"
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0 blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:((float)((rgbValue & 0xFF000000) >> 24)) / 255.0];
 
--- ios/Classes/Util/NativeUtils/IBGAPM+PrivateAPIs.h
+++ ios/Classes/Util/NativeUtils/IBGAPM+PrivateAPIs.h
@@ -11,7 +11,15 @@
 
 @interface IBGAPM (PrivateAPIs)
 
+
 @property (class, atomic, assign) BOOL networkEnabled;
+/// `w3ExternalTraceIDEnabled` will be only true if APM and network are enabled
+@property (class, atomic, assign) BOOL w3ExternalTraceIDEnabled;
+/// `w3ExternalGeneratedHeaderEnabled` will be only true if  APM, network and w3ExternalTraceIDEnabled are true
+@property (class, atomic, assign) BOOL w3ExternalGeneratedHeaderEnabled;
+/// `w3CaughtHeaderEnabled` will be only true if  APM, network and w3ExternalTraceIDEnabled are true
+@property (class, atomic, assign) BOOL w3CaughtHeaderEnabled;
+
 
 /// `endScreenLoadingEnabled` will be only true if  APM, screenLoadingFeature.enabled and autoUITracesUserPreference are true
 @property (class, atomic, assign) BOOL endScreenLoadingEnabled;
