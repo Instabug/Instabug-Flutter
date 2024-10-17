322ea6e306e05e0f842fcd6ed3df6a5c10000644
Ahmed alaa <aalaa@instabug.com>
2024-10-18 02:35:45 +0300

    refactor: make mono-repo

---
+++ packages/Instabug-Flutter/ios/Classes/Modules/InstabugPrivateViewApi.m
@@ -0,0 +1,8 @@
+#import "Instabug.h"
+#import "InstabugPrivateViewApi.h"
+#import "ArgsRegistry.h"
+
+//extern void InitInstabugPrivateViewApi(id<FlutterBinaryMessenger> messenger) {
+//    InstabugPrivateViewApi *api = [[InstabugPrivateViewApi alloc] init];
+//}
+
