fa720ff9ff5cd19ad2d0b6ff51991a3d9095626d
Mohamed Kamal <m.kamalgamil@gmail.com>
2025-03-06 14:53:27 +0200

    ci: disable verbose logging for curl commands

--- scripts/releases/get_job_approver.sh
+++ scripts/releases/get_job_approver.sh
@@ -1,7 +1,7 @@
 echo "CIRCLE_WORKFLOW_ID: $CIRCLE_WORKFLOW_ID"
 echo "CIRCLE_TOKEN: ${CIRCLE_TOKEN: -4}"  # (or at least echo if it's empty or not)
 
-jobsJson=$(curl -v -X GET "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job" --header "Circle-Token: $CIRCLE_TOKEN")
+jobsJson=$(curl -s -X GET "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job" --header "Circle-Token: $CIRCLE_TOKEN")
 
 echo "jobsJson response:"
 echo "$jobsJson"
@@ -18,7 +18,7 @@ echo "$approver_id"
 
 approver_id=$(echo "$approver_id" | tr -d '"')
 
-user=$(curl -v -X GET "https://circleci.com/api/v2/user/$approver_id" --header "Circle-Token: $CIRCLE_TOKEN")
+user=$(curl -s -X GET "https://circleci.com/api/v2/user/$approver_id" --header "Circle-Token: $CIRCLE_TOKEN")
 
 echo "user:"
 echo "$user"
