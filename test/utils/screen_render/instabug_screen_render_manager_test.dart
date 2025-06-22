ec2b04bb913d5b8d9fbcb5e5b4831e88d198d11a
Andrew Amin <adaniel@instabug.com>
2025-06-22 17:18:03 +0300

    chore:  add instabug_screen_render_manager_test_manual_mocks.dart

--- test/utils/screen_render/instabug_screen_render_manager_test.dart
+++ test/utils/screen_render/instabug_screen_render_manager_test.dart
@@ -1,19 +1,14 @@
-import 'dart:ui';
 
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:instabug_flutter/instabug_flutter.dart';
-import 'package:instabug_flutter/src/generated/apm.api.g.dart';
 import 'package:instabug_flutter/src/models/instabug_frame_data.dart';
 import 'package:instabug_flutter/src/models/instabug_screen_render_data.dart';
 
 import 'package:instabug_flutter/src/utils/screen_rendering/instabug_screen_render_manager.dart';
-import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'instabug_screen_render_manager_test.mocks.dart';
+import 'instabug_screen_render_manager_test_manual_mocks.dart';
 
-@GenerateMocks([FrameTiming, ApmHostApi, WidgetsBinding])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
