6f1ae20d079da21d7a1db9094795d1cf43b42924
Mohamed Kamal <m.kamalgamil@gmail.com>
2025-08-05 23:57:41 +0300

    chore: fix dart lint

--- lib/src/modules/crash_reporting.dart
+++ lib/src/modules/crash_reporting.dart
@@ -2,7 +2,6 @@
 
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:instabug_flutter/src/generated/crash_reporting.api.g.dart';
